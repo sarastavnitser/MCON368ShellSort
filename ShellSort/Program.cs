384ec5699f5d69d33dcea0e03b80d3354cd214fc
sarastavnitser <sstavnit@student.touro.edu>
2022-04-14 00:51:25 -0500

    started method for shell sort for int[]

--- ShellSort/Program.cs
+++ ShellSort/Program.cs
@@ -3,6 +3,31 @@
 namespace ShellSort{
     class ShellSort
     {
-        
+        public Array Sort(int[] givenArray)
+        {
+            int arrLen = givenArray.Length;
+            int gap = arrLen / 2;
+            while (gap != 1)
+            {
+                int left = 0;
+                int right = gap;
+                for (int i = 0; i < gap; i++)
+                {
+                    if (givenArray[left] > givenArray[right])
+                    {
+                        int lHolder = givenArray[left];
+                        int rHolder = givenArray[right];
+                        givenArray[left] = rHolder;
+                        givenArray[right] = lHolder;
+                    }
+
+                    left++;
+                    right++;
+                }
+
+                gap /= 2;
+            }
+            
+        }
     }
 }
